6a826519cdeeda23450641bcb3ccb64b8b40ccdf
Zhanbo1at <sharypkhanovzhanbolat@gmail.com>
2021-11-03 16:42:13 +0600

    Zhanbolatаенн

--- src/Class/Main.java
+++ src/Class/Main.java
@@ -8,7 +8,9 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Student[] student = new Student[]{new Student(1, "Zhndos", "07.11.2002", "Nur", "FIT", "2", "SIB-27"), new Student(1, "Zhndar", "07.11.2003", "Alm", "FEN", "2", "IS-27"), new Student(1, "Zhnbol", "07.11.2004", "Kokwetau", "FIT", "1", "SIB-17")};
+        Student[] student = new Student[]{new Student(1, "Zhndos", "07.11.2002", "Nur", "FIT", "2", "SIB-27"),
+                            new Student(1, "Zhndar", "07.11.2003", "Alm", "FEN", "2", "IS-27"),
+                            new Student(1, "Zhnbol", "07.11.2004", "Kokwetau", "FIT", "1", "SIB-17")};
         System.out.println("a) Studennter tyzymy korse facul");
         String facl = sc.nextLine();
 
